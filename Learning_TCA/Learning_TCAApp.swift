4e7ae22698ad8b294923022e7608f82f07d09a04
Ibrahim Hosseini <sehosseini@me.com>
2024-06-05 17:18:35 +0330

    Initial Commit

---
+++ Learning_TCA/Learning_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_TCAApp.swift
+//  Learning_TCA
+//
+//  Created by Ibrahim on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Learning_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
