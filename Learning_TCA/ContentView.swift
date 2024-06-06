01a1f119d667dbb9287f4dbbde50d2ca6a11a0a2
Ibrahim Hosseini <sehosseini@me.com>
2024-06-06 18:30:42 +0330

    state management

--- Learning_TCA/ContentView.swift
+++ Learning_TCA/ContentView.swift
@@ -6,19 +6,69 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @ObservedObject var state: AppState
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                NavigationLink(destination: CounterView(state: state)) {
+                    Text("Counter View")
+                }
+                
+                NavigationLink(destination: EmptyView()) {
+                         Text("Favorite primes")
+                }
+            }
+            .navigationBarTitle("State management")
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(state: AppState())
+}
+
+class AppState: ObservableObject {
+    @Published var count: Int = 0
+}
+
+struct CounterView: View {
+    
+    @ObservedObject var state: AppState
+    
+    private func ordinal(_ n: Int) -> String {
+      let formatter = NumberFormatter()
+      formatter.numberStyle = .ordinal
+      return formatter.string(for: n) ?? ""
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    state.count -= 1
+                }) {
+                    Text("-")
+                }
+                Text("\(state.count)")
+                Button(action: {
+                    state.count += 1
+                }) {
+                    Text("+")
+                }
+            }
+            
+            Button(action: {}) {
+                Text("Is this prime?")
+            }
+            Button(action: {}) {
+                Text("What is the \(self.ordinal(state.count)) prime?")
+            }
+        }
+        .font(.title)
+        .navigationTitle("Counter Demo")
+    }
 }
